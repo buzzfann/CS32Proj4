2c2a0b63ac1f2f1b9e5a85f3a7d6af34cc204b2d
buzzfann <78663982+buzzfann@users.noreply.github.com>
2021-03-07 15:41:44 -0800

    Update StudentTextEditor.cpp

--- StudentTextEditor.cpp
+++ StudentTextEditor.cpp
@@ -1,71 +1,131 @@
-//
-//  StudentTextEditor.cpp
-//  Project4
-//
-//  Created by Buzz Fann on 3/3/21.
-//
-
 #include "StudentTextEditor.h"
 #include "Undo.h"
 #include <string>
 #include <vector>
+#include <list>
+#include <map>
+#include <fstream>
+
+using namespace std;
 
 TextEditor* createTextEditor(Undo* un)
 {
-    return new StudentTextEditor(un);
+	return new StudentTextEditor(un);
 }
 
 StudentTextEditor::StudentTextEditor(Undo* undo)
  : TextEditor(undo) {
-    // TODO
+	// TODO
+     undObj = undo;
+     posX = 0;
+     posY = 0;
 }
 
 StudentTextEditor::~StudentTextEditor()
 {
-    // TODO
+    // iterate through the list/map, deleting everything along the way
+	// TODO
 }
 
 bool StudentTextEditor::load(std::string file) {
+    ifstream infile(file);
+    if (!infile)
+    {
+        return false;
+    }
+    string s;
+    std::list<std::string>::iterator it = textList.begin();
+    *it = "";
+    while (getline(infile, s))
+    {
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '\r')
+            {
+                break;
+            }
+            *it += s[i];
+        }
+        textList.push_back(*it);
+        it++;
+    }
     return false;  // TODO
 }
 
 bool StudentTextEditor::save(std::string file) {
-    return false;  // TODO
+    ofstream outfile(file);
+    if ( ! outfile )           // Did the creation fail?
+    {
+        return false;
+    }
+    string s;
+    std::list<std::string> temp = textList;
+    while (!temp.empty())
+    {
+        outfile << temp.front() << "\n";
+        temp.pop_front();
+    }
+    return true;
 }
 
 void StudentTextEditor::reset() {
-    // TODO
+	// TODO
+    posX = 0;
+    posY = 0;
+    textList.clear();
 }
 
 void StudentTextEditor::move(Dir dir) {
-    // TODO
+	// TODO
 }
 
 void StudentTextEditor::del() {
-    // TODO
+	// TODO
 }
 
 void StudentTextEditor::backspace() {
-    // TODO
+	// TODO
 }
 
 void StudentTextEditor::insert(char ch) {
-    // TODO
+	// TODO
 }
 
 void StudentTextEditor::enter() {
-    // TODO
+	// TODO
 }
 
 void StudentTextEditor::getPos(int& row, int& col) const {
-    // TODO
+	// TODO
+    row = posX;
+    col = posY;
 }
 
-void StudentTextEditor::getLines(int startRow, int numRows, std::vector<std::string>& lines) const {
-    // TODO
+int StudentTextEditor::getLines(int startRow, int numRows, std::vector<std::string>& lines) const
+{
+    if (startRow < 0 || numRows < 0 || startRow >= lines.size())
+    {
+        return -1;
+    }
+    if (startRow == lines.size() - 1)
+    {
+        lines.clear();
+        return 0;
+    }
+    std::vector<std::string> temp;
+    std::vector<std::string>::const_iterator start = lines.cbegin() + startRow;
+//    std::vector<std::string>::const_iterator temporary = start;
+    int i = 0;
+    for (; numRows > 0 && start != lines.cend(); start++, numRows--, i++)
+    {
+        std::string s = *start;
+//        start = lines.erase(start);
+        temp.push_back(s);
+    }
+    lines.clear();
+    lines = temp;
+    return i;
 }
 
 void StudentTextEditor::undo() {
-    // TODO
+	// TODO
 }
-
