e665b31d64c18f56b350f7802d113a7916d5f53a
buzzfann <78663982+buzzfann@users.noreply.github.com>
2021-03-07 15:42:04 -0800

    Update StudentTextEditor.h

--- StudentTextEditor.h
+++ StudentTextEditor.h
@@ -1,14 +1,9 @@
-//
-//  StudentTextEditor.hpp
-//  Project4
-//
-//  Created by Buzz Fann on 3/3/21.
-//
-
 #ifndef STUDENTTEXTEDITOR_H_
 #define STUDENTTEXTEDITOR_H_
 
 #include "TextEditor.h"
+#include <list>
+#include <string>
 
 class Undo;
 
@@ -16,22 +11,26 @@ class StudentTextEditor : public TextEditor {
 public:
 
     StudentTextEditor(Undo* undo);
-    ~StudentTextEditor();
-    bool load(std::string file);
-    bool save(std::string file);
-    void reset();
-    void move(Dir dir);
-    void del();
-    void backspace();
-    void insert(char ch);
-    void enter();
-    void getPos(int& row, int& col) const;
-    void getLines(int startRow, int numRows, std::vector<std::string>& lines) const;
-    void undo();
+	~StudentTextEditor();
+	bool load(std::string file);
+	bool save(std::string file);
+	void reset();
+	void move(Dir dir);
+	void del();
+	void backspace();
+	void insert(char ch);
+	void enter();
+	void getPos(int& row, int& col) const;
+	int getLines(int startRow, int numRows, std::vector<std::string>& lines) const;
+	void undo();
 
 private:
-
+    std::list<std::string> textList;
+    int posX;
+    int posY = 0;
+    Undo* undObj;
+    
+    
 };
 
 #endif // STUDENTTEXTEDITOR_H_
-
